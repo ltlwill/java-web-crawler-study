b2318983bc16f4286f575158c68a2ce61e201b99
ltlwill <609738992@qq.com>
2019-12-30 13:55:30 +0800

    by ltl

--- src/test/java/com/efe/ms/test/demo/BaseCrawler.java
+++ src/test/java/com/efe/ms/test/demo/BaseCrawler.java
@@ -1,5 +1,7 @@
 package com.efe.ms.test.demo;
 
+import java.util.logging.Logger;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -9,8 +11,18 @@ import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+
 public class BaseCrawler {
 	
+	protected Logger logger = Logger.getLogger(BaseCrawler.class.getName());
+	
+	/**
+	 * chrome 浏览器驱动
+	 * @return
+	 */
 	protected WebDriver chromeDriver() {
 		String driverPath = "E:\\softWareInstall\\webDriver\\chromedriver.exe";
 //		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
@@ -18,6 +30,10 @@ public class BaseCrawler {
 		return new ChromeDriver(); // chrome 
 	}
 	
+	/**
+	 * chrome headless模式驱动
+	 * @return
+	 */
 	protected WebDriver chromeHeadlessDriver() {
 		String driverPath = "E:\\softWareInstall\\webDriver\\chromedriver.exe";
 		System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, driverPath);
@@ -27,16 +43,40 @@ public class BaseCrawler {
 		return new ChromeDriver(opts); // chrome --headless模式（无界面模式）
 	}
 	
+	/**
+	 * firefox浏览器驱动
+	 * @return
+	 */
 	protected WebDriver firefoxDriver() {
 		String driverPath = "E:\\softWareInstall\\webDriver\\geckodriver.exe";
 		System.setProperty(GeckoDriverService.GECKO_DRIVER_EXE_PROPERTY, driverPath);
 		return new FirefoxDriver();
 	}
 	
+	/**
+	 * phantomjs 无界面浏览器驱动
+	 * @return
+	 */
 	protected WebDriver phantomjsDriver() {
 		String driverPath = "E:\\softWareInstall\\webDriver\\phantomjs.exe";
 		System.setProperty(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, driverPath);
 		return new PhantomJSDriver();
+	}
+	
+	/**
+	 * 创建htmlunit WebClient
+	 * @return
+	 */
+	protected WebClient htmlUnitWebClient() {
+		WebClient webClient = new WebClient(BrowserVersion.CHROME);
+		webClient.getOptions().setCssEnabled(false); // 无界面，不需要开启CSS
+		webClient.getOptions().setDownloadImages(false); // 不需要加载图片
+		webClient.getOptions().setJavaScriptEnabled(true); // 有ajax请求，需要开启
+		webClient.getOptions().setActiveXNative(false);
+		webClient.getOptions().setThrowExceptionOnScriptError(false); // 当JS执行出错的时候是否抛出异常, 这里选择不需要
+		webClient.getOptions().setThrowExceptionOnFailingStatusCode(false); // 当HTTP的状态非200时是否抛出异常, 这里选择不需要
+		webClient.setAjaxController(new NicelyResynchronizingAjaxController()); // 设置支持AJAX
+		return webClient;
 	}
 
 }
---
+++ src/test/java/com/efe/ms/test/demo/GiteeHtmlUnitCrawler.java
@@ -0,0 +1,78 @@
+package com.efe.ms.test.demo;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebWindow;
+import com.gargoylesoftware.htmlunit.html.DomAttr;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+/**
+ * 使用htmlunit 框架爬取gitee.com信息
+ * @author Tianlong Liu
+ * @2019年12月27日 下午5:03:02
+ */
+public class GiteeHtmlUnitCrawler extends BaseCrawler{
+	
+	@Test
+	public void test1() {
+		WebClient client = null;
+		try {
+			client = htmlUnitWebClient();
+			// 登录
+			login(client);
+			Thread.sleep(5000);
+			// 解析目标页面
+			parsePage(client);
+		}catch (Exception e) {
+			logger.info(e.getMessage()); 
+		}finally {
+			if(client != null) {
+				client.close();
+			}
+		}
+	}
+	
+	private void login(WebClient client) throws Exception{
+		String loginPageUrl = "https://gitee.com/login";
+		HtmlPage loginPage = client.getPage(loginPageUrl);
+		client.waitForBackgroundJavaScript(1000 * 15);
+		
+//		DomElement ele = loginPage.querySelector("#new_user #user_login"); // 用户名
+//		ele.setNodeValue("x@qq.com");
+//		ele = loginPage.querySelector("#new_user #user_password"); // 密码
+//		ele.setNodeValue("xx");
+//		ele = loginPage.querySelector("#new_user input[name=\"commit\"]"); // 提交表单按钮
+//		return ele.click(); // 点击登录
+		
+		HtmlInput name = loginPage.getFirstByXPath("//input[@id='user_login']");
+		name.setValueAttribute("xa@qq.com");
+		HtmlInput pwd = loginPage.getFirstByXPath("//input[@id='user_password']");
+		pwd.setValueAttribute("taxax");
+		HtmlInput btn = loginPage.getFirstByXPath("//input[@name='commit']");
+		btn.click();
+	}
+	
+	private void parsePage(WebClient client) throws Exception{
+		String pageUrl = "https://gitee.com/iiteemo/dashboard";
+		HtmlPage page = client.getPage(pageUrl);
+		client.waitForBackgroundJavaScript(1000 * 15);
+		DomNodeList<DomNode> nodes = page.querySelectorAll("#recommend-project__container .items .item");
+		int idx = 0;
+		nodes.forEach(node -> {
+			DomNode title = node.querySelector(".recommend-list .recommend-project__name a");
+			DomNode subTitle = node.querySelector(".recommend-list .recommend-project__describe");
+			System.out.println(node.getIndex() + ":" + title.getTextContent() + " | " 
+					+ subTitle.getTextContent() + " | " 
+					+ ((DomAttr)title.getAttributes().getNamedItem("href")).getValue()); 
+		});
+	}
+}
